040731c1947bb1ab8ed492519c83e9745878eeb4
IngCarrillo12 <IngCarrillo12>
2024-06-05 21:53:18 -0500

    VALIDANDO la cookie

--- BackendBookings/src/middlewares/validateToken.js
+++ BackendBookings/src/middlewares/validateToken.js
@@ -3,8 +3,8 @@ import { SECRETKEYPASSWORD } from "../config.js"
 
 export const authRequired = (req, res, next)=>{
     const {token} = req.cookies
-    console.log(req.cookies)
-    console.log('req',req)
+    console.log(req.cookies);
+    console.log('req',req);
     if(!token) return res.status(401).send({message: 'autorizacion denegada'})
     jwt.verify(token,SECRETKEYPASSWORD,(error,user)=>{
         if(error)return res.status(401).send({message: 'Token invalido'})
