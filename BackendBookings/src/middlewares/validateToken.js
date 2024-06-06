9526d7e266c02d7cf2623830d74f42129bd1e745
IngCarrillo12 <IngCarrillo12>
2024-06-05 22:03:07 -0500

    Bug cookie trabjando

--- BackendBookings/src/middlewares/validateToken.js
+++ BackendBookings/src/middlewares/validateToken.js
@@ -2,9 +2,10 @@ import jwt from "jsonwebtoken"
 import { SECRETKEYPASSWORD } from "../config.js"
 
 export const authRequired = (req, res, next)=>{
-    const {token} = req.cookies
+    const { token } = req.cookies
     console.log('cookie', req.cookies)
-    console.log('req',req)
+    console.log('req',req.cookie)
+    console.log('TOKEN', token)
     if(!token) return res.status(401).send({message: 'autorizacion denegada'})
     jwt.verify(token,SECRETKEYPASSWORD,(error,user)=>{
         if(error)return res.status(401).send({message: 'Token invalido'})
